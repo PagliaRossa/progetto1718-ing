2f9063d6968d6997c125a916e5352958f31be120
Donato <ulissis1996@gmail.com>
2018-04-25 11:32:53 +0200

    Add Analysis class and Implement help

---
+++ src/main/java/it/uniba/analysis/Analysis.java
@@ -0,0 +1,30 @@
+package it.uniba.analysis;
+
+class Analysis {
+	
+	//TODO
+	//void usersList();
+	
+	//TODO
+	//void channelList();
+	
+	//TODO
+	//void usersSortedByChannel();
+	
+	//TODO
+	//void channelMembers(String channel);
+	
+	void help() {
+		String help = new String();
+		
+		help += "These are all available command for sna4slack\n\n";
+		help += "usersList                               Show users list\n";
+		help += "channelList                             Show channel list\n";
+		help += "usersSortedByChannel                    Show users sorted by channel\n";
+		help += "channelMembers channelname              Show users member of seleceted channel\n";
+		help += "sna4slack                               Show this help interface";
+		
+		System.out.println(help);
+	}
+
+}
