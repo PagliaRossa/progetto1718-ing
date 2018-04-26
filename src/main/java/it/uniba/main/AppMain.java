176fad8b86994d1df25a00e2fddbaf537e662eee
Donato Leone <ulissis1996@gmail.com>
2018-04-26 16:14:43 +0200

    Revert "Issue#5"

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -1,8 +1,5 @@
 package it.uniba.main;
 
-import java.util.Scanner;
-import it.uniba.analysis.*;
-
 /**
  * The main class for the project. It must be customized to meet the project
  * assignment specifications.
@@ -25,18 +22,7 @@ public final class AppMain {
 	 *            The command-line arguments.
 	 */
 	public static void main(final String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Analysis request = new Analysis();
-		System.out.println("Welcome in sna4slack , insert command into next line");
-		while(true) {
-			String command = scanner.nextLine();
-			if (command.equals("sna4slack"))
-				request.help();
-			else if (command.equals("exit"))
-				break;
-			else if ((!command.equals("sna4slack") && (!command.equals("exit"))))
-				System.out.println("Wrong command , use sna4slack command for help");
-		}
+		System.out.println("Hello world.");
 	}
 
 }
