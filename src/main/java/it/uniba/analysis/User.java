83e8f3dcd0340a665626399e433831092a033a88
Donato <ulissis1996@gmail.com>
2018-04-26 20:31:31 +0200

    Add User class that represents user information from json

---
+++ src/main/java/it/uniba/analysis/User.java
@@ -0,0 +1,17 @@
+package it.uniba.analysis;
+
+class User {
+	
+	private String id;
+	private String name;
+	
+	void setId(String id) {
+		this.id = id;
+	}
+	void setName(String name) {
+		this.name = name;
+	}
+	String getId() { return this.id;}
+	String getName() { return this.name;}
+	
+}
