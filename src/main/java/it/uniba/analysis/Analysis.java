c9c4ecdc804d0547fc22523af03f6600324fb31f
Donato <ulissis1996@gmail.com>
2018-04-26 20:20:38 +0200

    Add new userList command help in help

--- src/main/java/it/uniba/analysis/Analysis.java
+++ src/main/java/it/uniba/analysis/Analysis.java
@@ -6,6 +6,7 @@ public class Analysis {
 		String help = new String();
 		
 		help += "These are all available command for sna4slack\n\n";
+		help += "usersList  zipUrl                 Show users list in selected workspace with zipUrl";
 		help += "sna4slack                         Show this help interface";
 		
 		System.out.println(help);
