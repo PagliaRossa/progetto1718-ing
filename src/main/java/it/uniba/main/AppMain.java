4da9146981fca780575cf0ef410a38b048375db6
Donato <ulissis1996@gmail.com>
2018-05-14 15:33:46 +0200

    New controls in AppMain

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -1,6 +1,14 @@
 package it.uniba.main;
 
 import it.uniba.analysis.Analysis;
+import it.uniba.analysis.ChannelsAnalysis;
+import it.uniba.analysis.MembersAnalysis;
+import it.uniba.analysis.MentionsAnalysis;
+import it.uniba.data.Channel;
+import it.uniba.util.ChannelsPrinter;
+import it.uniba.util.Help;
+import it.uniba.util.MembersPrinter;
+import it.uniba.util.MentionsPrinter;
 
 /**
  * The main class for the project. It must be customized to meet the project
@@ -24,12 +32,14 @@ public final class AppMain {
 	 *            The command-line arguments.
 	 */
 	public static void main(final String[] command) {
-		Analysis request = new Analysis();
+		Analysis request;
 		if (!(command.length == 0)) {
 			if (command[0].equals("membersList")) {
 				if (command.length != 1) {
-					if (request.membersList(command[1])) {
-						System.out.println(request.printMembers());
+					request = new MembersAnalysis();
+					if (((MembersAnalysis) request).membersList(command[1])) {
+						MembersPrinter printer = new MembersPrinter();
+						System.out.println(printer.print(request.getMembers()));
 					}
 				} else {
 					System.out.println("Command incomplete , use sna4slack for help");
@@ -38,8 +48,10 @@ public final class AppMain {
 			
 			if (command[0].equals("channelsList")) {
 				if (command.length != 1) {
-					if (request.channelsList(command[1])) {
-						System.out.println(request.printChannels());
+					request = new ChannelsAnalysis();
+					if (((ChannelsAnalysis) request).channelsList(command[1])) {
+						ChannelsPrinter printer = new ChannelsPrinter();
+						System.out.println(printer.print(((ChannelsAnalysis)request).getChannels()));
 					}
 				} else {
 					System.out.println("Command incomplete , use sna4slack for help");
@@ -48,7 +60,12 @@ public final class AppMain {
 			
 			if (command[0].equals("membersChannel")) {
 				if (command.length == 3) {
-					request.membersChannel(command[1],command[2]);
+					request = new ChannelsAnalysis();
+					if (((ChannelsAnalysis) request).membersChannel(command[1],command[2])) {
+						ChannelsPrinter printer = new ChannelsPrinter();
+						Channel channel = ((ChannelsAnalysis)request).getChannel(command[1]);
+						System.out.println(printer.memberInChannelPrint(channel,(ChannelsAnalysis)request,command[1]));
+					}
 				} else {
 					System.out.println("Command incomplete , use sna4slack for help");
 				}
@@ -56,7 +73,11 @@ public final class AppMain {
 			
 			if (command[0].equals("membersSortedByChannel")) {
 				if (command.length != 1) {
-					request.membersSortedByChannel(command[1]);
+					request = new ChannelsAnalysis();
+					if (((ChannelsAnalysis)request).membersSortedByChannel(command[1])) {
+						ChannelsPrinter printer = new ChannelsPrinter();
+						System.out.println(printer.membersSortedByChannelPrint((ChannelsAnalysis)request));
+					}
 				} else {
 					System.out.println("Command incomplete , use sna4slack for help");
 				}
@@ -66,66 +87,142 @@ public final class AppMain {
 				if (command.length == 1) {
 					System.out.println("Command incomplete , use sna4slack for help");
 				} else {
-					if (request.mentionsList(command[1])) {
-						request.membersList(command[1]);
-						System.out.println(request.printMentionsList());
+					request = new MentionsAnalysis();
+					if (((MentionsAnalysis) request).mentionsList(command[1])) {
+						if (!((MentionsAnalysis) request).isEmpty()) {
+							MembersAnalysis members = new MembersAnalysis();
+							members.membersList(command[1]);
+							request.setMembers(members.getMembers());
+							((MentionsAnalysis) request).setNameFromTo();
+							MentionsPrinter printer = new MentionsPrinter();
+							System.out.println(printer.print(((MentionsAnalysis)request).getMentions()));
+						} else {
+							System.out.println("Can't find mentions");
+						}
 					}
 				}
 			}
+			
 			if (command[0].equals("mentionsListChannel")) {
 				if (command.length == 3) {
-					if (request.mentionsListChannel(command[1],command[2])) {
-						request.membersList(command[2]);
-						System.out.println(request.printMentionsList());
+					request = new MentionsAnalysis();
+					if (((MentionsAnalysis) request).mentionsListChannel(command[1],command[2])) {
+						if (!((MentionsAnalysis) request).isEmpty()) {
+							MembersAnalysis members = new MembersAnalysis();
+							members.membersList(command[2]);
+							request.setMembers(members.getMembers());
+							((MentionsAnalysis) request).setNameFromTo();
+							MentionsPrinter printer = new MentionsPrinter();
+							System.out.println(printer.print(((MentionsAnalysis) request).getMentions()));
+						} else {
+							System.out.println("Can't find mentions");
+						}
 					}
 				} else {
 					System.out.println("Command incomplete , use sna4slack for help");
 				}
 			}
+			
 			if (command[0].equals("mentionsListFrom")) {
 				if (command.length == 3) {
-					if (request.mentionsList(command[2])) {
-						request.membersList(command[2]);
-						System.out.println(request.printMentionsFrom(command[1]));
+					request = new MentionsAnalysis();
+					if (((MentionsAnalysis)request).mentionsList(command[2])) {
+						if (!((MentionsAnalysis) request).isEmpty()) {
+							MembersAnalysis members = new MembersAnalysis();
+							members.membersList(command[2]);
+							if (members.isInList(command[1])) {
+								request.setMembers(members.getMembers());
+								((MentionsAnalysis) request).setNameFromTo();
+								MentionsPrinter printer = new MentionsPrinter();
+								System.out.println(printer.printFrom(((MentionsAnalysis)request).getMentions(),command[1]));
+							} else {
+								System.out.println("Member not found");
+							}
+						} else {
+							System.out.println("Can't find mentions");
+						}
 					}
 				} else if (command.length == 4) {
-					if (request.mentionsListChannel(command[2],command[3])) {
-						request.membersList(command[3]);
-						System.out.println(request.printMentionsFrom(command[1]));
+					request = new MentionsAnalysis();
+					if (((MentionsAnalysis)request).mentionsListChannel(command[2],command[3])) {
+						if (!((MentionsAnalysis) request).isEmpty()) {
+							MembersAnalysis members = new MembersAnalysis();
+							members.membersList(command[3]);
+							if (members.isInList(command[1])){
+								request.setMembers(members.getMembers());
+								((MentionsAnalysis) request).setNameFromTo();
+								MentionsPrinter printer = new MentionsPrinter();
+								System.out.println(printer.printFrom(((MentionsAnalysis)request).getMentions(),command[1]));
+							} else {
+								System.out.println("Member not found");
+							}
+						} else {
+							System.out.println("Can't find mentions");
+						}
 					}
 				} else {
 					System.out.println("Command incomplete , use sna4slack for help");
 				}
 			}
+			
 			if (command[0].equals("mentionsListTo")) {
 				if (command.length == 3) {
-					if (request.mentionsList(command[2])) {
-						request.membersList(command[2]);
-						System.out.println(request.printMentionsTo(command[1]));
+					request = new MentionsAnalysis();
+					if (((MentionsAnalysis)request).mentionsList(command[2])) {
+						if (!((MentionsAnalysis) request).isEmpty()) {
+							MembersAnalysis members = new MembersAnalysis();
+							members.membersList(command[2]);
+							if (members.isInList(command[1])) {
+								request.setMembers(members.getMembers());
+								((MentionsAnalysis) request).setNameFromTo();
+								MentionsPrinter printer = new MentionsPrinter();
+								System.out.println(printer.printTo(((MentionsAnalysis)request).getMentions(),command[1]));
+							} else {
+								System.out.println("Member not found");
+							}
+						} else {
+							System.out.println("Can't find mentions");
+						}
 					}
 				} else if (command.length == 4) {
-					if (request.mentionsListChannel(command[2],command[3])) {
-						request.membersList(command[3]);
-						System.out.println(request.printMentionsTo(command[1]));
+					request = new MentionsAnalysis();
+					if (((MentionsAnalysis)request).mentionsListChannel(command[2],command[3])) {
+						if (!((MentionsAnalysis) request).isEmpty()) {
+							MembersAnalysis members = new MembersAnalysis();
+							members.membersList(command[3]);
+							if (members.isInList(command[1])) {
+								request.setMembers(members.getMembers());
+								((MentionsAnalysis) request).setNameFromTo();
+								MentionsPrinter printer = new MentionsPrinter();
+								System.out.println(printer.printTo(((MentionsAnalysis)request).getMentions(),command[1]));
+							} else {
+								System.out.println("Member not found");
+							}
+						} else {
+							System.out.println("Can't find mentions");
+						}
 					}
 				} else {
 					System.out.println("Command incomplete , use sna4slack for help");
 				}
 			}
+			
 			if (command[0].equals("sna4slack")) {
 				if (command.length == 1) {
-					request.help();
+					Help help = new Help();
+					System.out.println(help);
 				} else {
 					System.out.println("No need for more argument , just use sna4slack for help");
 				}
 			}
 			
 			if (!command[0].equals("membersList") && !command[0].equals("channelsList")
-				&& !command[0].equals("membersChannel") && !command[0].equals("membersSortedByChannel")
-				&& !command[0].equals("sna4slack") && !command[0].equals("mentionsList") && !command[0].equals("mentionsListChannel")
-				&& !command[0].equals("mentionsListFrom") && !command[0].equals("mentionsListTo")) {
-					System.out.println("Wrong command, use sna4slack command for help");
-			}
+					&& !command[0].equals("membersChannel") && !command[0].equals("membersSortedByChannel")
+					&& !command[0].equals("sna4slack") && !command[0].equals("mentionsList") && !command[0].equals("mentionsListChannel")
+					&& !command[0].equals("mentionsListFrom") && !command[0].equals("mentionsListTo")) {
+						System.out.println("Wrong command, use sna4slack command for help");
+				}
+			
 		} else {
 			System.out.println("Needs arguments , use sna4slack for help");
 		}
