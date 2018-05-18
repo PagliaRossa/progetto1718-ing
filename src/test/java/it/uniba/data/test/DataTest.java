b34b6c673822774d6d940e6e4e402bc1fda3d941
Donato <ulissis1996@gmail.com>
2018-05-18 21:44:40 +0200

    Remove unused method , add new tests

--- src/test/java/it/uniba/data/test/DataTest.java
+++ src/test/java/it/uniba/data/test/DataTest.java
@@ -14,13 +14,14 @@ import org.junit.jupiter.api.Test;
 import it.uniba.data.Channel;
 import it.uniba.data.Member;
 import it.uniba.data.Mention;
+import it.uniba.data.Counter;
 
 
 public class DataTest {
 	
 	@Test
-	@DisplayName("Test data")
-	void Data() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	@DisplayName("Test channel")
+	void Channel() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		Channel channel = new Channel("9","marco",new ArrayList<>());
 		assertTrue(channel.isEmpty());
 		assertEquals(new ArrayList<>(),channel.getMembers());
@@ -40,24 +41,50 @@ public class DataTest {
 		field = channel.getClass().getDeclaredField("members");
 		field.setAccessible(true);
 		assertEquals(field.get(channel),members);
+	}
+	
+	@Test
+	@DisplayName("Test member")
+	void Member() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		Member member = new Member("0","mimmo");
 		member.setId("1");
-		field = member.getClass().getDeclaredField("identificator");
+		Field field = member.getClass().getDeclaredField("identificator");
 		field.setAccessible(true);
 		assertEquals(field.get(member),"1");
 		member.setName("donato");
 		field = member.getClass().getDeclaredField("name");
 		field.setAccessible(true);
 		assertEquals(field.get(member),"donato");
+	}
+	
+	@Test
+	@DisplayName("Test mention")
+	void Mention() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		Mention mention = new Mention("From","To");
 		mention.setFrom("me");
-		field = mention.getClass().getDeclaredField("fromMember");
+		Field field = mention.getClass().getDeclaredField("fromMember");
 		field.setAccessible(true);
 		assertEquals(field.get(mention),"me");
 		mention.setTo("you");
 		field = mention.getClass().getDeclaredField("toMember");
 		field.setAccessible(true);
 		assertEquals(field.get(mention),"you");
+	}
+	
+	@Test
+	@DisplayName("Test Counter")
+	void Counter() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Counter counter = new Counter(10,10);
+		assertEquals(10,counter.getIndex());
+		assertEquals(10,counter.getOccurenceNumber());
+		counter.setIndex(11);
+		Field field = counter.getClass().getDeclaredField("index");
+		field.setAccessible(true);
+		assertEquals(field.get(counter),11);
+		counter.setOccurenceNumber(11);
+		field = counter.getClass().getDeclaredField("occurenceNumber");
+		field.setAccessible(true);
+		assertEquals(field.get(counter),11);
 	}
 
 }
