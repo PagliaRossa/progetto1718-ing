977ed6be2cc98e4b8bb6a309abb23453d472975b
Donato <ulissis1996@gmail.com>
2018-05-19 04:19:35 +0200

    Fix pmd problems in unit test

--- src/test/java/it/uniba/util/test/JSONTest.java
+++ src/test/java/it/uniba/util/test/JSONTest.java
@@ -10,22 +10,50 @@ import org.junit.jupiter.api.Test;
 import it.uniba.util.JSONReader;
 import it.uniba.util.ZipReader;
 
+@SuppressWarnings("PMD.TooManyStaticImports")
 public class JSONTest {
 	
+	static String fail = "fail";
+	
 	@Test
-	@DisplayName("Test json")
-	void JSON() {
-		JSONReader json = new JSONReader();
-		ZipReader zip = new ZipReader();
+	@DisplayName("Test setMembers json")
+	void members() {
+		final JSONReader json = new JSONReader();
+		final ZipReader zip = new ZipReader();
 		try {
 			assertNotNull(json.setMembers(zip.setUsersFile("res/file/test.zip")));
-			assertNotNull(json.setChannels(zip.setChannelsFile("res/file/test.zip")));
-			assertNotNull(json.setMentions(zip.getJSONFromFile("res/file/test.zip","bernerslee/2018-03-13.json")));
-			
 		} catch (ParseException p) {
-			System.out.println("fail");
+			System.out.println(fail);
 		} catch (IOException i) {
-			System.out.println("fail");
+			System.out.println(fail);
+		}
+	}
+	
+	@Test
+	@DisplayName("Test setChannelsFile json")
+	void channels() {
+		final JSONReader json = new JSONReader();
+		final ZipReader zip = new ZipReader();
+		try {
+			assertNotNull(json.setChannels(zip.setChannelsFile("res/file/test.zip")));
+		} catch (ParseException p) {
+			System.out.println(fail);
+		} catch (IOException i) {
+			System.out.println(fail);
+		}
+	}
+	
+	@Test
+	@DisplayName("Test random file json")
+	void randomfile() {
+		final JSONReader json = new JSONReader();
+		final ZipReader zip = new ZipReader();
+		try {
+			assertNotNull(json.setMentions(zip.getJSONFromFile("res/file/test.zip","bernerslee/2018-03-13.json")));
+		} catch (ParseException p) {
+			System.out.println(fail);
+		} catch (IOException i) {
+			System.out.println(fail);
 		}
 	}
 
