f627a253dbe98ded0f43a320c6b7835346ffe4e6
Donato <ulissis1996@gmail.com>
2018-05-22 18:06:54 +0200

    Add javadoc for main package

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -12,6 +12,9 @@ import it.uniba.control.Controller;
  */
 public final class AppMain {
 
+	/**
+	 * Boolean that checks if main returns true or false.
+	 */
 	private static boolean check;
 	/**
 	 * Private constructor. Change if needed.
@@ -20,6 +23,10 @@ public final class AppMain {
 
 	}
 
+	/**
+	 * Get check.
+	 * @return check - True or false
+	 */
 	public static boolean isCheck() {
 		return check;
 	}
@@ -34,6 +41,11 @@ public final class AppMain {
 		check = checkBoolean(command);
 	}
 
+	/**
+	 * Check returning boolean of Controller showRequest.
+	 * @param command command-line arguments
+	 * @return boolean - true or false
+	 */
 	public static boolean checkBoolean(final String... command) {
 		if (command.length > 0) {
 			final Controller controller = new Controller(Arrays.copyOf(command, command.length));
